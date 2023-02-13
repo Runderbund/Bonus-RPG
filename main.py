22d6fe9d54e00e64f99701bc970849ed3194edcd
unknown <sean.herbison@gmail.com>
2023-02-13 10:44:05 -0700

    Adds functions to allow user attack choice and do battle.

--- main.py
+++ main.py
@@ -12,7 +12,20 @@ def make_creatures():
     rodent_of_unusual_size = {"health":20, "att_power": 3, "attacks":["Bite", "Chew", "Gnaw", "Nibble"]}
     return hercules, troll, gryphon, rodent_of_unusual_size
 
-def battle_troll():
-    pass
+def battle_monster(hercules, monster):
+    while hercules["health"] > 0 and troll["health"] > 0: #Herc always wins, right now
+        att_choice = hercules["attacks"][offer_att_option()]
+        print (f"Hercules attacks troll with {att_choice} for 25 damage!")
+        troll["health"] -= 25
+        if troll["health"] > 0:
+            troll_attack = troll["attacks"][random.randint(0, 3)]
+            print (f"Troll attacks Hercules with {troll_attack} for 5 damage!")
+            hercules["health"] -= 25
+
+def offer_att_option(hercules):
+    for i, attack in enumerate(hercules["attacks"], 1):
+        print(f"{i}: {attack}")
+    user_choice = int(input("Which attack should Hercules use? ")) - 1
+    return user_choice
 
 main()
