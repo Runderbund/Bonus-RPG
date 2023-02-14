ad2585e33f6cf3f89f76ac61bb341e0b85327601
unknown <sean.herbison@gmail.com>
2023-02-14 10:16:34 -0700

    Makes minor efficiency tweaks, saves a few lines

--- main.py
+++ main.py
@@ -14,38 +14,33 @@ def make_creatures():
     hercules = {"health":100, "att_power": 25, "attacks":["Punch", "Headbutt", "Kick", "Stern Gaze"]}
     troll = {"health":100, "att_power": 5, "attacks":["Grunt", "Throw Club", "Bash", "Step"], "name":"troll"}
     gryphon = {"health":50, "att_power": 15, "attacks":["Slash", "Kick", "Bite", "Tackle"],"name":"gryphon"}
-    rodent_of_unusual_size = {"health":20, "att_power": 3, "attacks":["Bite", "Chew", "Gnaw", "Nibble"], "name":"Rodent of Unusual size"}
+    rodent_of_unusual_size = {"health":20, "att_power": 3, "attacks":["Bite", "Chew", "Gnaw", "Nibble"], "name":"Rodent of Unusual Size"}
     # I'm assuming it's fine to add a name to the dictionary
     return hercules, troll, gryphon, rodent_of_unusual_size
 
 def battle_monster(hercules, monster):
-    while hercules["health"] > 0 and monster["health"] > 0: #Herc always wins, right now
+    while hercules["health"] > 0 and monster["health"] > 0:
         hercules_attack(hercules, monster)
-        monster_attack(hercules, monster)
+        if monster["health"] > 0:
+            monster_attack(hercules, monster)
         
 def offer_att_option(hercules):
     print ("\nHercules' attacks:")
     for i, attack in enumerate(hercules["attacks"], 1):
         print(f"{i}: {attack}")
     user_choice = int(input("\nWhich attack should Hercules use? ")) - 1
-    # Could add input checking
     return user_choice
 
 def hercules_attack(hercules, monster):
-    att_choice = hercules["attacks"][offer_att_option(hercules)]
-    att_power = hercules["att_power"]
-    monster_name = monster["name"]
-    print (f"\nHercules attacks the {monster_name} with {att_choice} for {att_power} damage!")
+    attack_choice = hercules["attacks"][offer_att_option(hercules)]
+    print (f"\nHercules attacks the {monster['name']} with {attack_choice} for {hercules['att_power']} damage!")
         # Why can't I do "...attacks the {monster["name"]} with ..."
-    monster["health"] -= att_power
+    monster["health"] -= hercules['att_power']
 
 def monster_attack(hercules, monster):
-    monster_name = monster["name"]
-    att_power = monster["att_power"]
-    if monster["health"] > 0:
-        attack_choice = monster["attacks"][random.randint(0, 3)]
-        print (f"The {monster_name} attacks Hercules with {attack_choice} for {att_power} damage!")
-        hercules["health"] -= att_power
+    attack_choice = monster["attacks"][random.randint(0, 3)]
+    print (f"The {monster['name']} attacks Hercules with {attack_choice} for {monster['att_power']} damage!")
+    hercules["health"] -= monster['att_power']
 
 def tell_story():
     hercules, troll, gryphon, rous = make_creatures()
