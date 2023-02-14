f82c8dde750c2ef691c1e20f5f97b1241b05a66d
unknown <sean.herbison@gmail.com>
2023-02-14 09:56:36 -0700

    Simple, deterministic RPG tale. Working, if boring.

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import random
 
 def main():
     hercules, troll, gryphon, rous = make_creatures()
-    battle_monster(hercules, troll)
-    # print (hercules["health"])
-
+    tell_story(hercules, troll, gryphon, rous)
 
 def make_creatures():
-    hercules = {"health":100, "att_power": 125, "attacks":["Punch", "Headbutt", "Kick", "Stern Gaze"]}
-    troll = {"health":150, "att_power": 5, "attacks":["Grunt", "Throw Club", "Bash", "Step"]}
-    gryphon = {"health":50, "att_power": 15, "attacks":["Slash", "Kick", "Bite", "Tackle"]}
-    rodent_of_unusual_size = {"health":20, "att_power": 3, "attacks":["Bite", "Chew", "Gnaw", "Nibble"]}
+    hercules = {"health":100, "att_power": 25, "attacks":["Punch", "Headbutt", "Kick", "Stern Gaze"]}
+    troll = {"health":100, "att_power": 5, "attacks":["Grunt", "Throw Club", "Bash", "Step"], "name":"troll"}
+    gryphon = {"health":50, "att_power": 15, "attacks":["Slash", "Kick", "Bite", "Tackle"],"name":"gryphon"}
+    rodent_of_unusual_size = {"health":20, "att_power": 3, "attacks":["Bite", "Chew", "Gnaw", "Nibble"], "name":"Rodent of Unusual size"}
+    # I'm assuming it's fine to add a name to the dictionary
     return hercules, troll, gryphon, rodent_of_unusual_size
 
 def battle_monster(hercules, monster):
@@ -18,29 +17,44 @@ def battle_monster(hercules, monster):
         hercules_attack(hercules, monster)
         monster_attack(hercules, monster)
         
-
 def offer_att_option(hercules):
     print ("\nHercules' attacks:")
     for i, attack in enumerate(hercules["attacks"], 1):
         print(f"{i}: {attack}")
-    user_choice = int(input("Which attack should Hercules use? ")) - 1
+    user_choice = int(input("\nWhich attack should Hercules use? ")) - 1
     return user_choice
 
 def hercules_attack(hercules, monster):
     att_choice = hercules["attacks"][offer_att_option(hercules)]
     att_power = hercules["att_power"]
-    print (f"\nHercules attacks monster with {att_choice} for {att_power} damage!")
-    # Not sure how to print dict name without overcomplicating this.
-        # Leaving generic "monster" for now
+    monster_name = monster["name"]
+    print (f"\nHercules attacks {monster_name} with {att_choice} for {att_power} damage!")
+    # Why can't I do "...attacks {monster["name"]} with ..."
     monster["health"] -= att_power
 
 def monster_attack(hercules, monster):
+    monster_name = monster["name"]
+    att_power = monster["att_power"]
     if monster["health"] > 0:
         attack_choice = monster["attacks"][random.randint(0, 3)]
-        att_power = monster["att_power"]
-        print (f"Monster attacks Hercules with {attack_choice} for {att_power} damage!")
+        print (f"The {monster_name} attacks Hercules with {attack_choice} for {att_power} damage!")
         hercules["health"] -= att_power
-    else:
-        print ("Hercules has defeated the monster!")
+
+def tell_story(hercules, troll, gryphon, rous):
+    print ("Behold, the mighty Hercules sets forth on a mighty adventure!")
+    
+    print ("On his travels throughout the land, Hercules is accosted by a troll!")
+    battle_monster(hercules, troll)
+    print ("\nThe troll proves no match for Hercules!\n")
+
+    print ("Upon climbing to the top of a mountain, Hercules is suddenly attacked by a flurry of feathers!")
+    battle_monster(hercules, gryphon)
+    print ("\nThe gryphon too falls under Hercules mighty swings.\n")
+
+
+    print ("Finally, Hercules passes through the fire swamp, where a shadow lunges at him!")
+    battle_monster(hercules, rous)
+    print ("\nHercules smashes the rodent and proves yet again that he fears no creature.\n")
+    print ("Victorious, Hercules grabs a burrito, and once again wonders why he put his fridge so far away.")
 
 main()
